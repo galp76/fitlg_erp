b867c89d41f5ab1e97fd3bb3497cbb2ba15f2e7c
u0_a233 <germanlobo76@gmail.com>
2024-08-25 14:18:47 -0400

    Commit de actualizacion del repositorio antes de comenzar a desarrollar los modulos del sistema

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
+static mut VALIDATED: bool = false;
+
 #[macro_use] extern crate rocket;
 
 #[get("/")]
 fn auth() -> rocket::response::content::RawHtml<String> {
-    rocket::response::content::RawHtml(std::fs::read_to_string("auth.xhtml").unwrap())
+    rocket::response::content::RawHtml(std::fs::read_to_string("xhtml/auth.xhtml").unwrap())
 }
 
 #[get("/index")]
 fn index() -> rocket::response::content::RawHtml<String> {
-    rocket::response::content::RawHtml(std::fs::read_to_string("hello.xhtml").unwrap())
+    unsafe {
+        if VALIDATED == true {
+            rocket::response::content::RawHtml(std::fs::read_to_string("xhtml/index.xhtml").unwrap())
+        } else {
+            rocket::response::content::RawHtml(std::fs::read_to_string("xhtml/auth.xhtml").unwrap())
+        }
+    }
 }
 
 #[get("/process/<password>")]
 fn process(password: &str) -> rocket::response::Redirect {
     println!("\nProcesando password: {}\n", password);
     if password == "26082011" {
+        unsafe {
+            VALIDATED = true;
+        }
         rocket::response::Redirect::to(uri!(index))
     } else {
         rocket::response::Redirect::to(uri!(auth))
